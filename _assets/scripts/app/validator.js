c90b7b573df2cb01be038de7bf48701cc34c79a9
smutnyleszek <smutnyleszek@gmail.com>
2016-02-20 11:59:44 +0100

    finished validator with tests

--- _assets/scripts/app/validator.js
+++ _assets/scripts/app/validator.js
@@ -12,41 +12,56 @@
         return re.test(testString);
     };
 
+    // Check if string contains no digit characters.
+    // @param {string} [testString] string to be tested
+    AppValidator.prototype.testHasNoDigits = function (testString) {
+        var re = new RegExp('^([^0-9]*)$', 'g');
+        return re.test(testString);
+    };
+
+    // Check if string has only digit characters.
+    // @param {string} [testString] string to be tested
+    AppValidator.prototype.testHasOnlyDigits = function (testString) {
+        var re = new RegExp('^[0-9]+$', 'g');
+        return re.test(testString);
+    };
+
     // Checks if string contains any letter characters.
     // @param {string} [testString] string to be tested
     AppValidator.prototype.testHasLetters = function (testString) {
-        var re = new RegExp('\\w', 'g');
+        var re = new RegExp('[a-zA-Z\\u00C0-\\u017F]', 'g');
         return re.test(testString);
     };
 
     // Checks if string contains any special characters.
     // @param {string} [testString] string to be tested
     AppValidator.prototype.testHasSpecials = function (testString) {
-        return false;
+        var re = new RegExp('[^a-zA-Z\\d\\s:]', 'g');
+        return re.test(testString);
     };
 
+    // Checks if string is longer than given limit.
     // @param {string} [testString] string to be tested
-    // @param {number} {}
-    AppValidator.prototype.testLengthOver = function (testString) {
-        return false;
+    // @param {integer} {limit} that string needs to be longer than
+    AppValidator.prototype.testLengthOver = function (testString, limit) {
+        return testString.length > limit;
     };
 
+    // Checks if string is shorter than given limit.
     // @param {string} [testString] string to be tested
-    // @param {number} {}
-    AppValidator.prototype.testLengthUnder = function (testString) {
-        return false;
+    // @param {integer} {limit} that string needs to be shorter than
+    AppValidator.prototype.testLengthUnder = function (testString, limit) {
+        return testString.length < limit;
     };
 
-    // @param {string} [testString] string to be tested
-    // @param {number} {}
-    AppValidator.prototype.testNumberRange = function (testString) {
-        return false;
-    };
-
-    // @param {string} [testString] string to be tested
-    // @param {number} {}
-    AppValidator.prototype.testDigitsCount = function (testString) {
-        return false;
+    // Check if number is withing a range (with edges).
+    // @param {integer} [testNumber] number to be tested
+    // @param {integer} [min] bottom edge
+    // @param {integer} [max] top edge
+    AppValidator.prototype.testNumberRange = function (testNumber, min, max) {
+        var isOverMin = testNumber >= min;
+        var isUnderMax = testNumber <= max;
+        return isOverMin && isUnderMax;
     };
 
     // export to app
--- _assets/scripts/tests/app/validatorSpec.js
+++ _assets/scripts/tests/app/validatorSpec.js
@@ -42,6 +42,71 @@ describe('validator', function () {
 
     });
 
+    describe('testHasNoDigits method', function () {
+
+        it('should exist', function () {
+            expect(app.validator.testHasNoDigits).toBeDefined();
+        });
+
+        it('should succeed for string without digits', function () {
+            var strings = [
+                'pójdźże, kiń tę chmurność w głąb flaszy',
+                'pterodactyl porn',
+                'Ol',
+                '~!@#$%^&*()_+'
+            ];
+            for (var i = 0; i < strings.length; i += 1) {
+                expect(app.validator.testHasNoDigits(strings[i])).toBeTruthy();
+            }
+        });
+
+        it('should fail for string with digits', function () {
+            var strings = [
+                'wziąłbym 1 pęczek lub 7 gwoździków',
+                'pr0n',
+                '1',
+                '~!@345^&*90-+'
+            ];
+            for (var i = 0; i < strings.length; i += 1) {
+                expect(app.validator.testHasNoDigits(strings[i])).toBeFalsy();
+            }
+        });
+
+    });
+
+    describe('testHasOnlyDigits method', function () {
+
+        it('should exist', function () {
+            expect(app.validator.testHasOnlyDigits).toBeDefined();
+        });
+
+        it('should fail for string with non-digit characters', function () {
+            var strings = [
+                'pójdźże, kiń tę chmurność w głąb flaszy',
+                'pterodactyl porn',
+                'Ol',
+                '~!@#$%^&*()_+',
+                '11231241 12312',
+                '3,14'
+            ];
+            for (var i = 0; i < strings.length; i += 1) {
+                expect(app.validator.testHasOnlyDigits(strings[i])).toBeFalsy();
+            }
+        });
+
+        it('should succeed for string with digits only', function () {
+            var strings = [
+                '1337',
+                '2345678831265698657432',
+                '1'
+            ];
+            for (var i = 0; i < strings.length; i += 1) {
+                expect(app.validator.testHasOnlyDigits(strings[i])).toBeTruthy();
+            }
+        });
+
+    });
+
     describe('testHasLetters method', function () {
 
         it('should exist', function () {
@@ -120,12 +185,54 @@ describe('validator', function () {
             expect(app.validator.testLengthOver).toBeDefined();
         });
 
+        it('should fail for equal length string', function () {
+            expect(app.validator.testLengthOver('~!@345^&*9O-+', 13)).toBeFalsy();
+            expect(app.validator.testLengthOver('8=D(|)', 6)).toBeFalsy();
+            expect(app.validator.testLengthOver('pr0n', 4)).toBeFalsy();
+            expect(app.validator.testLengthOver('', 0)).toBeFalsy();
+        });
+
+        it('should fail for shorter string', function () {
+            expect(app.validator.testLengthOver('~!@345^&*9O-+', 14)).toBeFalsy();
+            expect(app.validator.testLengthOver('8=D(|)', 99)).toBeFalsy();
+            expect(app.validator.testLengthOver('pr0n', 100)).toBeFalsy();
+            expect(app.validator.testLengthOver('', 897659876)).toBeFalsy();
+        });
+
+        it('should succeed for longer string', function () {
+            expect(app.validator.testLengthOver('~!@345^&*9O-+', 12)).toBeTruthy();
+            expect(app.validator.testLengthOver('8=D(|)', 1)).toBeTruthy();
+            expect(app.validator.testLengthOver('pr0n', 2)).toBeTruthy();
+            expect(app.validator.testLengthOver('', -1)).toBeTruthy();
+        });
+
     });
 
     describe('testLengthUnder method', function () {
 
         it('should exist', function () {
             expect(app.validator.testLengthUnder).toBeDefined();
+        });
+
+        it('should fail for equal length string', function () {
+            expect(app.validator.testLengthUnder('~!@345^&*9O-+', 13)).toBeFalsy();
+            expect(app.validator.testLengthUnder('8=D(|)', 6)).toBeFalsy();
+            expect(app.validator.testLengthUnder('pr0n', 4)).toBeFalsy();
+            expect(app.validator.testLengthUnder('', 0)).toBeFalsy();
+        });
+
+        it('should fail for longer string', function () {
+            expect(app.validator.testLengthUnder('~!@345^&*9O-+', 12)).toBeFalsy();
+            expect(app.validator.testLengthUnder('8=D(|)', 1)).toBeFalsy();
+            expect(app.validator.testLengthUnder('pr0n', 2)).toBeFalsy();
+            expect(app.validator.testLengthUnder('', -1)).toBeFalsy();
+        });
+
+        it('should succeed for shorter string', function () {
+            expect(app.validator.testLengthUnder('~!@345^&*9O-+', 14)).toBeTruthy();
+            expect(app.validator.testLengthUnder('8=D(|)', 99)).toBeTruthy();
+            expect(app.validator.testLengthUnder('pr0n', 100)).toBeTruthy();
+            expect(app.validator.testLengthUnder('', 897659876)).toBeTruthy();
         });
 
     });
@@ -136,16 +243,32 @@ describe('validator', function () {
             expect(app.validator.testNumberRange).toBeDefined();
         });
 
-        it('should fail for empty', function () {
-            expect(app.validator.testNumberRange('')).toBeFalsy();
+        it('should succeed for number within range', function () {
+            expect(app.validator.testNumberRange(0,-3,3)).toBeTruthy();
+            expect(app.validator.testNumberRange(133,89,34567)).toBeTruthy();
+            expect(app.validator.testNumberRange(2,1,3)).toBeTruthy();
+            expect(app.validator.testNumberRange(11,10,12)).toBeTruthy();
         });
 
-    });
+        it('should succeed for number on the edge', function () {
+            expect(app.validator.testNumberRange(-3,-3,3)).toBeTruthy();
+            expect(app.validator.testNumberRange(89,89,34567)).toBeTruthy();
+            expect(app.validator.testNumberRange(3,1,3)).toBeTruthy();
+            expect(app.validator.testNumberRange(12,10,12)).toBeTruthy();
+        });
 
-    describe('testDigitsCount method', function () {
+        it('should fail for number under range', function () {
+            expect(app.validator.testNumberRange(-6,-3,3)).toBeFalsy();
+            expect(app.validator.testNumberRange(88,89,34567)).toBeFalsy();
+            expect(app.validator.testNumberRange(0,1,3)).toBeFalsy();
+            expect(app.validator.testNumberRange(9,10,12)).toBeFalsy();
+        });
 
-        it('should exist', function () {
-            expect(app.validator.testDigitsCount).toBeDefined();
+        it('should fail for number over range', function () {
+            expect(app.validator.testNumberRange(4,-3,3)).toBeFalsy();
+            expect(app.validator.testNumberRange(34568,89,34567)).toBeFalsy();
+            expect(app.validator.testNumberRange(4,1,3)).toBeFalsy();
+            expect(app.validator.testNumberRange(13,10,12)).toBeFalsy();
         });
 
     });
